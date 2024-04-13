24bbe1b04e8c6ff05737abe62f39d61a0da9f3eb
Dheeraj Meka <dheeraj.meka@outlook.com>
2024-04-13 15:20:21 -0400

    a6 Mongo sp24

--- App.js
+++ App.js
@@ -5,13 +5,45 @@ import cors from "cors";
 import CourseRoutes from './Kanbas/courses/routes.js';
 import ModuleRoutes from './Kanbas/modules/routes.js';
 import AssignmentRoutes from './Kanbas/assignments/routes.js';
+import mongoose from 'mongoose';
+import UserRoutes from './Kanbas/users/routes.js';
+import session from 'express-session';
+import "dotenv/config";
+/*
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+mongoose.connect("mongodb+srv://kanbas_admin:AyhzsXPa74EiycRn@kanbas.rl59jhj.mongodb.net/?retryWrites=true&w=majority&appName=kanbas");
+ */
+const CONNECTION_STRING = process.env.REACT_APP_KANBAS_DB_CONNECTION_STRING;
+const DB_NAME = process.env.REACT_APP_DB_NAME;
+mongoose.connect(CONNECTION_STRING, { dbName: DB_NAME, useNewUrlParser: true, useUnifiedTopology: true });
+
 
 const app = express();
-app.use(cors());  
+app.use(cors({
+    credentials: true,
+    origin: [process.env.FRONTEND_URL, "http://localhost:3000"]
+  }));
+  const sessionOptions = {
+    secret: 'abcdef',
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+      domain: process.env.HTTP_SERVER_DOMAIN,
+    };
+  }
+  app.use(session(sessionOptions));
+  
+  
 app.use(express.json());
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
+UserRoutes(app);
 Lab5(app);
 Hello(app);
 app.listen(process.env.PORT || 4000);
--- Kanbas/Database/assignments.js
+++ Kanbas/Database/assignments.js
@@ -2,7 +2,7 @@ export default [
   {
   "_id": "A101",
   "title": "Propulsion Assignment",
-  "course": "RS101",
+  "course": "661891647dd407fddaea7885",
   "desc": "This Assignment covers Propulsion Concept.",
   "points": 100,
   "dueDate": "2022-10-08",
@@ -12,7 +12,7 @@ export default [
   {
   "_id": "A102",
   "title": "Combustion Analysis",
-  "course": "RS101",
+  "course": "661891647dd407fddaea7885",
   "desc": "This Assignment covers Combustion Analysis.",
   "points": 100,
   "dueDate": "2022-09-25",
@@ -22,7 +22,7 @@ export default [
   {
   "_id": "A103",
   "title": "Nozzle Design Project",
-  "course": "RS101",
+  "course": "661891647dd407fddaea7885",
   "desc": "This Assignment covers Nozzle Design Project.",
   "points": 100,
   "dueDate": "2022-10-02",
@@ -32,7 +32,7 @@ export default [
   {
   "_id": "A201",
   "title": "Aerodynamics Quiz",
-  "course": "RS102",
+  "course": "661891797dd407fddaea7887",
   "desc": "This Assignment covers Aerodynamics Concept.",
   "points": 100,
   "dueDate": "2022-09-25",
@@ -42,7 +42,7 @@ export default [
   {
   "_id": "A202",
   "title": "Flow Analysis",
-  "course": "RS102",
+  "course": "661891797dd407fddaea7887",
   "desc": "This Assignment covers Flow Analysis",
   "points": 100,
   "dueDate": "2022-10-25",
@@ -52,7 +52,7 @@ export default [
   {
   "_id": "A203",
   "title": "Heating Analysis",
-  "course": "RS102",
+  "course": "661891797dd407fddaea7887",
   "desc": "This Assignment covers Heating Concept.",
   "points": 100,
   "dueDate": "2022-09-25",
@@ -62,7 +62,7 @@ export default [
   {
   "_id": "A301",
   "title": "Structural Design Task",
-  "course": "RS103",
+  "course": "661891937dd407fddaea7889",
   "desc": "This Assignment covers Structural Design Concept.",
   "points": 100,
   "dueDate": "2022-11-18",
@@ -72,7 +72,7 @@ export default [
   {
   "_id": "A302",
   "title": "Orbital Calculations",
-  "course": "RS103",
+  "course": "661891937dd407fddaea7889",
   "desc": "This Assignment covers Orbital Calculations.",
   "points": 100,
   "dueDate": "2022-09-25",
@@ -82,7 +82,7 @@ export default [
   {
   "_id": "A303",
   "title": "Systems Engineering Exam",
-  "course": "RS103",
+  "course": "661891937dd407fddaea7889",
   "desc": "This Assignment covers Systems Engineering Concept.",
   "points": 100,
   "dueDate": "2022-09-25",
